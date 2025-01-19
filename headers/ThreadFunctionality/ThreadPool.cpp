a0f96091d251aaa3b654fa84f24da4285903a71f
Jacob Pagan <jacob.pagan10@gmail.com>
2025-01-19 10:30:35 -0600

    ...

--- headers/ThreadFunctionality/ThreadPool.cpp
+++ headers/ThreadFunctionality/ThreadPool.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "ThreadPool.h"
+
+void ThreadPool::Start() {
+    const uint32_t numberOfThreads = std::thread::hardware_concurrency() / 2;
+
+    for (uint32_t i = 0; i < numberOfThreads; ++i) {
+        threads_.emplace_back(std::thread(&ThreadPool::ThreadLoop, this));
+    }
+}
+
+void ThreadPool::ThreadLoop() {
+    while (true) {
+        std::function<void()> job;
+        {
+            std::unique_lock<std::mutex> lock(queueMutex_);
+            mutexCondition_.wait(lock, [this] {
+              return !jobs_.empty() || should_terminate;
+            });
+
+            if (should_terminate) {
+                return;
+            }
+
+            job = jobs_.front();
+            jobs_.pop();
+        }
+
+        job();
+    }
+}
